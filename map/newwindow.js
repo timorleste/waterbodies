18989868b7330f0cb684cf7eebd97fdbcd2b4ef1
timorleste <Think@Tonys-MacBook-Pro.local>
2016-12-25 22:18:46 +0900

    T
    T

---
+++ map/newwindow.js
@@ -0,0 +1,9 @@
+<!-- 
+  function PopUpNewWindow(targetURL)
+  {
+     NewWindow =
+		window.open(targetURL,'',
+'width=403,height=200,locationbar=yes,menubar=no,scrollbars=yes,status=no,resizable=no,screenX=600,screenY=150');
+		NewWindow.focus();
+  }
+//-->
